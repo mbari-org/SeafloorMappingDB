e7a665b057194561517d9617e2fafdb6fc8130c4
Mike McCann <mccann@mbari.org>
2021-05-25 16:15:29 -0700

    Add Admin interfaces for all smdb classes

--- smdb/smdb/admin.py
+++ smdb/smdb/admin.py
@@ -1,3 +1,75 @@
-from django.contrib import admin
+from django.contrib.gis.admin import GeoModelAdmin, register
 
-# Register your models here.
+from smdb.models import (Citation, CitationMission, Compilation, DataArchival,
+                         DataArchivalMission, Expedition, Mission, MissionType,
+                         Person, Platform, PlatformType, Sensor, SensorMission,
+                         SensorType,)
+
+@register(Citation)
+class CitationAdmin(GeoModelAdmin):
+    pass
+
+
+@register(CitationMission)
+class CitationMissionAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Compilation)
+class CompilationAdmin(GeoModelAdmin):
+    pass
+
+
+@register(DataArchival)
+class DataArchivalAdmin(GeoModelAdmin):
+    pass
+
+
+@register(DataArchivalMission)
+class DataArchivalMissionAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Expedition)
+class ExpeditionAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Mission)
+class MissionAdmin(GeoModelAdmin):
+    pass
+
+
+@register(MissionType)
+class MissionTypeAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Person)
+class PersonAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Platform)
+class PlatformAdmin(GeoModelAdmin):
+    pass
+
+
+@register(PlatformType)
+class PlatformTypeAdmin(GeoModelAdmin):
+    pass
+
+
+@register(Sensor)
+class SensorAdmin(GeoModelAdmin):
+    pass
+
+
+@register(SensorMission)
+class SensorMissionAdmin(GeoModelAdmin):
+    pass
+
+
+@register(SensorType)
+class SensorTypeAdmin(GeoModelAdmin):
+    pass
