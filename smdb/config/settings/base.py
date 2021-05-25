09ef756270d0f056a1b34a63b40216631c5e1872
Mike McCann <mccann@mbari.org>
2021-05-25 15:37:33 -0700

    Add django.contrib.gis so as to get OpenLayers widget in admin interface

--- smdb/config/settings/base.py
+++ smdb/config/settings/base.py
@@ -63,6 +63,7 @@ DJANGO_APPS = [
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
+    "django.contrib.gis",
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
