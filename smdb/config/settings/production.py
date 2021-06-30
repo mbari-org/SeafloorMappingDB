e79fd55f8a2c175afc98e3a5cf6f4599881359c8
Mike McCann <mccanndocker_user@mbari.org>
2021-06-29 17:50:52 -0700

    Add django_extensions so that 'manage.py shell_plus' works

--- smdb/config/settings/production.py
+++ smdb/config/settings/production.py
@@ -171,5 +171,10 @@ LOGGING = {
     },
 }
 
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
+
 # Your stuff...
 # ------------------------------------------------------------------------------
