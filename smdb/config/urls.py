72bd451e8774092a73a70147d917148b5fd4ba58
Mike McCann <mccann@mbari.org>
2021-07-08 12:37:56 -0700

    Implement MissionOverView for 'Overview First' home page

--- smdb/config/urls.py
+++ smdb/config/urls.py
@@ -5,9 +5,10 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
+from views import MissionOverView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", MissionOverView.as_view(), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
