981e4e41d1413143683017add6d8024718ec1c0e
Mike McCann <mccann@mbari.org>
2021-07-09 16:41:15 -0700

    Add search field on home page

--- smdb/smdb/views.py
+++ smdb/smdb/views.py
@@ -17,12 +17,31 @@ class MissionOverView(TemplateView):
 
         """Return the view context data."""
         context = super().get_context_data(**kwargs)
-        missions = Mission.objects.all()
-        ##missions = Mission.objects.filter(mission_name__contains="lidar")
+        search_string = context["view"].request.GET.get("q")
+        if search_string:
+            missions = Mission.objects.filter(mission_name__icontains=search_string)
+        else:
+            missions = Mission.objects.all()
+
         self.logger.info(f"Serializing {missions.count()} missions to geojson...")
-        context["missions"] = json.loads(serialize("geojson", missions, fields=('grid_bounds', 'mission_name')))
+        context["missions"] = json.loads(
+            serialize("geojson", missions, fields=("id", "grid_bounds", "mission_name"))
+        )
         self.logger.info(f"# of Queries: {len(connection.queries)}")
-        self.logger.info((f"Size of context['missions']: {len(str(context['missions']))}"))
-        self.logger.debug(f"context['missions'] = { json.dumps(context['missions'], indent=4, sort_keys=True)}")
+        self.logger.info(
+            (f"Size of context['missions']: {len(str(context['missions']))}")
+        )
+        self.logger.debug(
+            f"context['missions'] = { json.dumps(context['missions'], indent=4, sort_keys=True)}"
+        )
+
+        if search_string:
+            context[
+                "search_string"
+            ] = f"{len(context['missions']['features'])} Missions containing '{search_string}'"
+        else:
+            context[
+                "search_string"
+            ] = f"All {len(context['missions']['features'])} Missions"
 
         return context
