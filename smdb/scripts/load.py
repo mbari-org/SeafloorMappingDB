6680c8846c8337821c6ee3e10dbeb364cc61c43e
Mike McCann <mccann@mbari.org>
2021-08-02 12:16:40 -0700

    Try grandparent directory for Notes.txt file

--- smdb/scripts/load.py
+++ smdb/scripts/load.py
@@ -126,6 +126,13 @@ class Scanner:
             for txt_file in subprocess.getoutput(locate_cmd).split("\n"):
                 self.logger.debug("Potential notes file: %s", txt_file)
                 notes_file = txt_file
+        if not notes_file:
+            # Try grandparent directory
+            grandparent_dir = os.path.abspath(os.path.join(sm_dir, "../.."))
+            locate_cmd = f"locate -d /etc/smdb/SeafloorMapping.db -r '{grandparent_dir}.*Notes.txt'"
+            for txt_file in subprocess.getoutput(locate_cmd).split("\n"):
+                self.logger.debug("Potential notes file: %s", txt_file)
+                notes_file = txt_file
 
         return notes_file
 
