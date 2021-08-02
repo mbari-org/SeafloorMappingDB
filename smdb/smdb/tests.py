8f28733aa922dd48fbb09d2122d3c025e63e2f42
Mike McCann <mccann@mbari.org>
2021-08-02 12:15:26 -0700

    Example test from https://djangostars.com/blog/django-pytest-testing/

--- smdb/smdb/tests.py
+++ smdb/smdb/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user("john", "lennon@thebeatles.com", "johnpassword")
+    assert User.objects.count() == 1
