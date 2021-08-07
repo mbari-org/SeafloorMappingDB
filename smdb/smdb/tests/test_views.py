fd100902a20e66dd58dee404520970a246f8e954
Mike McCann <mccann@mbari.org>
2021-08-07 12:31:11 -0700

    Django REST Framework api and tests implemented for MissionType

---
+++ smdb/smdb/tests/test_views.py
@@ -0,0 +1,67 @@
+'''import pytest
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.http import HttpRequest
+from django.test import RequestFactory
+from django.urls import reverse
+
+from smdb.models import Mission
+from smdb.tests.factories import MissionFactory
+from smdb.views import MissionOverView
+
+pytestmark = pytest.mark.django_db
+
+
+class TestMissionOverView:
+    """
+    TODO:
+        extracting view initialization code as class-scoped fixture
+        would be great if only pytest-django supported non-function-scoped
+        fixture db access -- this is a work-in-progress for now:
+        https://github.com/pytest-dev/pytest-django/pull/258
+    """
+
+    def dummy_get_response(self, request: HttpRequest):
+        return None
+
+    def test_get_success_url(self, user: User, rf: RequestFactory):
+        view = UserUpdateView()
+        request = rf.get("/fake-url/")
+        request.user = user
+
+        view.request = request
+
+        assert view.get_success_url() == f"/users/{user.username}/"
+
+    def test_get_object(self, user: User, rf: RequestFactory):
+        view = UserUpdateView()
+        request = rf.get("/fake-url/")
+        request.user = user
+
+        view.request = request
+
+        assert view.get_object() == user
+
+
+class TestUserDetailView:
+    def test_authenticated(self, user: User, rf: RequestFactory):
+        request = rf.get("/fake-url/")
+        request.user = UserFactory()
+
+        response = user_detail_view(request, username=user.username)
+
+        assert response.status_code == 200
+
+    def test_not_authenticated(self, user: User, rf: RequestFactory):
+        request = rf.get("/fake-url/")
+        request.user = AnonymousUser()
+
+        response = user_detail_view(request, username=user.username)
+        login_url = reverse(settings.LOGIN_URL)
+
+        assert response.status_code == 302
+        assert response.url == f"{login_url}?next=/fake-url/"
+'''
