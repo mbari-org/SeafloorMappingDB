3b9958cc2c4557a36132b4ca8b821b66b54be308
Mike McCann <mccann@mbari.org>
2021-08-16 23:19:53 +0000

    Add pytest fixture to load django fixture. Use this test data to implement a test for MissionOverView.

--- smdb/smdb/tests/test_views.py
+++ smdb/smdb/tests/test_views.py
@@ -1,67 +1,32 @@
-'''import pytest
+import pytest
 from django.conf import settings
-from django.contrib import messages
-from django.contrib.auth.models import AnonymousUser
-from django.contrib.messages.middleware import MessageMiddleware
-from django.contrib.sessions.middleware import SessionMiddleware
 from django.http import HttpRequest
-from django.test import RequestFactory
 from django.urls import reverse
 
-from smdb.models import Mission
-from smdb.tests.factories import MissionFactory
 from smdb.views import MissionOverView
 
 pytestmark = pytest.mark.django_db
 
-
-class TestMissionOverView:
-    """
-    TODO:
-        extracting view initialization code as class-scoped fixture
-        would be great if only pytest-django supported non-function-scoped
-        fixture db access -- this is a work-in-progress for now:
-        https://github.com/pytest-dev/pytest-django/pull/258
-    """
-
-    def dummy_get_response(self, request: HttpRequest):
-        return None
-
-    def test_get_success_url(self, user: User, rf: RequestFactory):
-        view = UserUpdateView()
-        request = rf.get("/fake-url/")
-        request.user = user
-
-        view.request = request
-
-        assert view.get_success_url() == f"/users/{user.username}/"
-
-    def test_get_object(self, user: User, rf: RequestFactory):
-        view = UserUpdateView()
-        request = rf.get("/fake-url/")
-        request.user = user
-
-        view.request = request
-
-        assert view.get_object() == user
+from django.utils.http import urlencode
 
 
-class TestUserDetailView:
-    def test_authenticated(self, user: User, rf: RequestFactory):
-        request = rf.get("/fake-url/")
-        request.user = UserFactory()
+def test_home_view(client):
+    url = reverse("home")
+    response = client.get(url)
+    assert response.status_code == 200
 
-        response = user_detail_view(request, username=user.username)
 
-        assert response.status_code == 200
+def test_num_missions(client):
+    url = reverse("home")
+    response = client.get(url)
+    missions = response.context["missions"]["features"]
+    # Depends on missions_notes_5.json fixture being loaded
+    assert len(missions) == 5
 
-    def test_not_authenticated(self, user: User, rf: RequestFactory):
-        request = rf.get("/fake-url/")
-        request.user = AnonymousUser()
 
-        response = user_detail_view(request, username=user.username)
-        login_url = reverse(settings.LOGIN_URL)
-
-        assert response.status_code == 302
-        assert response.url == f"{login_url}?next=/fake-url/"
-'''
+def test_search_missions(client):
+    url = reverse("home")
+    response = client.get(url, {"q": "20190308"})
+    missions = response.context["missions"]["features"]
+    # Depends on missions_notes_5.json fixture being loaded
+    assert len(missions) == 2
