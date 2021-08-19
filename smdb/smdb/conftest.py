86a2770ef013c73b64ac2e32c4e31390657bf831
Mike McCann <mccann@mbari.org>
2021-08-18 18:01:54 -0700

    Add docstring describing origin of missions_notes_5.json fixture

--- smdb/smdb/conftest.py
+++ smdb/smdb/conftest.py
@@ -13,6 +13,11 @@ from smdb.tests.factories import MissionTypeFactory, PersonFactory, PlatformType
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
+    """Create smdb/smdb/fixtures/missions_notes_5.json thusly:
+    1. Modify smdb/scripts/load.py to exit after saving 5 missions
+    2. python smdb/manage.py dumpdata -o smdb/smdb/fixtures/missions_notes_5.json
+    See: https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-database-with-initial-test-data
+    """
     with django_db_blocker.unblock():
         call_command("loaddata", "missions_notes_5.json")
 
