29cb9185a8f45de31a390978dac0661ef1975769
Mike McCann <mccann@mbari.org>
2021-08-21 05:32:10 +0000

    missiontypes -> missiontype

--- smdb/config/api_router.py
+++ smdb/config/api_router.py
@@ -2,7 +2,11 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from smdb.users.api.views import UserViewSet
-from smdb.api.views import MissionTypeViewSet, PersonViewSet, PlatformTypeViewSet
+from smdb.api.views import (
+    MissionTypeViewSet,
+    PersonViewSet,
+    PlatformTypeViewSet,
+)
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -11,7 +15,7 @@ else:
 
 router.register("users", UserViewSet)
 ##router.register("missions", MissionViewSet)
-router.register("missiontypes", MissionTypeViewSet)
+router.register("missiontype", MissionTypeViewSet, basename="missiontype")
 router.register("persons", PersonViewSet)
 router.register("platformtypes", PlatformTypeViewSet)
 
