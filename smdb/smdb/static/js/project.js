4baabffb8fb6d39f2828f4fe86ac367c8a5af666
Karen A. Salamy <salamy@mbari.org>
2022-05-25 13:23:42 -0700

    Update project.js
    Fixed leaflet-measure code bug introduced with Leaflet 1.8.0 update.

--- smdb/smdb/static/js/project.js
+++ smdb/smdb/static/js/project.js
@@ -34,3 +34,18 @@ function updateMap() {
   }
   window.location.search = searchParams.toString();
 }
+
+L.Control.Measure.include({
+  // set icon on the capture marker
+  _setCaptureMarkerIcon: function () {
+    // disable autopan
+    this._captureMarker.options.autoPanOnFocus = false;
+
+    // default function
+    this._captureMarker.setIcon(
+      L.divIcon({
+        iconSize: this._map.getSize().multiplyBy(2),
+      })
+    );
+  },
+});
