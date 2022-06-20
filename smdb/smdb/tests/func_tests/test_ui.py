2f0b485d2a7e80e0406d7f74ad43544e994c6d87
Mike McCann <mccann@mbari.org>
2022-06-20 23:20:12 +0000

    Use the simpler sample code for test_ui.py

--- smdb/smdb/tests/func_tests/test_ui.py
+++ smdb/smdb/tests/func_tests/test_ui.py
@@ -1,21 +1,9 @@
-import socket
-
-import pytest
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test import override_settings
+from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-pytestmark = pytest.mark.django_db
 
-
-@override_settings(ALLOWED_HOSTS=["*"])  # Disable ALLOW_HOSTS
-class BaseTestCase(StaticLiveServerTestCase):
-    """
-    Provides base test class which connects to the Docker
-    container running Selenium.
-    """
-
+class SeleniumTest(TestCase):
     def setUp(self):
         self.chrome = webdriver.Remote(
             command_executor="http://selenium-hub:4444/wd/hub",
@@ -24,16 +12,15 @@ class BaseTestCase(StaticLiveServerTestCase):
         self.chrome.implicitly_wait(10)
         self.server_url = "http://django:8001"
 
-
-class HomePageTest(BaseTestCase):
-    def test_home_page_open(self):
+    def test_visit_site_with_chrome(self):
         self.chrome.get(self.server_url)
         self.assertIn("SeafloorMappingDB", self.chrome.title)
         number = self.chrome.find_element(By.ID, "num-missions").text
         self.assertEqual("5", number)
 
-    def test_spatial_bounds_link(self):
-        self.chrome.find_element(By.ID, "use_bounds").click()
-        self.chrome.find_element(By.ID, "update-map").click()
-        # TODO: test for map bounds in url
-        self.assertIn("", self.chrome.current_url)
+    # def test_spatial_bounds_link(self):
+    #    self.chrome.get(self.server_url)
+    #    self.chrome.find_element(By.ID, "use_bounds").click()
+    #    self.chrome.find_element(By.ID, "update-map").click()
+    #    # TODO: test for map bounds in url
+    #    self.assertIn("", self.chrome.current_url)
