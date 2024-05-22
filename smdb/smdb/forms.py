b005bc4536842bfa9fe4621a68e155496cebcbaf
Mike McCann <MBARIMike@gmail.com>
2024-05-22 21:43:17 +0000

    Comment out patch_test filter as it's "too agressive"

--- smdb/smdb/forms.py
+++ smdb/smdb/forms.py
@@ -9,7 +9,7 @@ class MissionFilterFormHelper(FormHelper):
             Column("name", css_class="form-group col-md mb-0"),
             Column("region_name", css_class="form-group col-md mb-0"),
             Column("status", css_class="form-group col-md-1 mb-0"),
-            Column("patch_test", css_class="form-group col-md-1 mb-0"),
+            # Column("patch_test", css_class="form-group col-md-1 mb-0"),
             Column("mgds_compilation", css_class="form-group col-md mb-0"),
             Column("expedition__name", css_class="form-group col-md mb-0"),
             Column(Submit("submit", "Filter", css_class="col-md mb-0 btn-primary")),
