52141c91e012fd4ca99d34fa413a6716691fa95d
Mike McCann <MBARIMike@gmail.com>
2024-05-28 22:56:27 +0000

    Upgrade from bootstrap4 to bootstrap5

--- smdb/config/settings/base.py
+++ smdb/config/settings/base.py
@@ -73,7 +73,7 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
-    "crispy_bootstrap4",
+    "crispy_bootstrap5",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -210,10 +210,11 @@ TEMPLATES = [
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 
 # http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = ("bootstrap4", "bootstrap5", "uni_form")
 
 # https://django-tables2.readthedocs.io/en/latest/pages/custom-rendering.html#available-templates
-DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap4.html"
+DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap5.html"
 
 # FIXTURES
 # ------------------------------------------------------------------------------
